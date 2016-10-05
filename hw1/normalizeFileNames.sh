9cc3dc7e51d5eca85bf49580229d1c25dfc94f69
Anqi Shi <ashi6@wisc.edu>
2016-10-05 17:46:38 -0500

    normalize

--- hw1/normalizeFileNames.sh
+++ hw1/normalizeFileNames.sh
@@ -1,10 +1,10 @@
-#Code used to change filenames
+#Code used to change filenames for hw1 exercise1
 
 for i in {1..9}
 do
   #echo " i = $i"
   # mv $i ${i/timetest$i_snaq/timetest0$i_snaq}
-  #fn is the input filenames taht need changing 
+  #fn is the input filenames taht need changing
   fn=log/timetest${i}_snaq.log
   #echo "fn=$fn"
   fn2=log/timetest0${i}_snaq.log
